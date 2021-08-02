0d7708b585176103931ff9995d86f604c66b092e
Narmadha154 <85552355+Narmadha154@users.noreply.github.com>
2021-08-02 21:59:36 +0530

    Update Removal.java

--- src/program/Removal.java
+++ src/program/Removal.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class Removal {
     public void removeTheElement() {
-        int n,index;
+        int n,value;
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the array size:");
         n=sc.nextInt();
@@ -12,11 +12,22 @@ public class Removal {
         for(int i=0;i<n;i++)
             array[i]=sc.nextInt();
         System.out.println("Array elements are :" + Arrays.toString(array));
-        System.out.println("enter the index to be removed:");
-        index = sc.nextInt();
-        if (n - 1 - index >= 0) System.arraycopy(array, index + 1, array, index, n - 1 - index);
-        n=n-1;
-        int [] array1=Arrays.copyOf(array,n);
-        System.out.println("Resultant array is" + Arrays.toString(array1));
+        Arrays.sort(array);
+        System.out.println("sorted array is : " + Arrays.toString(array));
+        System.out.println("enter the value to be removed:");
+        value = sc.nextInt();
+        System.out.println("value is:"+ value);
+        int [] newarray=new int[n-1];
+        for(int i=0,j=0;i<n&j<n;i++,j++){
+            if (value == array[i]) {
+                newarray[j] = array[i + 1];
+                i++;
+            }
+            if(value!=array[i]){
+                newarray[j]=array[i];
+            }
+        }
+        for(int i=0;i<n-1;i++)
+            System.out.println("new array is :" + newarray[i]);
     }
 }
