d71fddc1ee2640891aacec499dcc6d911b419edd
Narmadha154 <85552355+Narmadha154@users.noreply.github.com>
2021-08-03 12:19:42 +0530

    Update Inserting.java

--- src/program/Inserting.java
+++ src/program/Inserting.java
@@ -1,7 +1,7 @@
 package program;
 import java.util.Arrays;
 import java.util.Scanner;
-public class Inserting {
+public class Insert {
     public void insertTheElement(){
         int n,value,index;
         Scanner sc=new Scanner(System.in);
@@ -9,8 +9,9 @@ public class Inserting {
         n=sc.nextInt();
         int [] array=new int[n];
         System.out.println("enter the elements:");
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++){
             array[i]=sc.nextInt();
+        }
         System.out.println("Array elements are :" + Arrays.toString(array));
         Arrays.sort(array);
         System.out.println("sorted array is : " + Arrays.toString(array));
@@ -20,18 +21,19 @@ public class Inserting {
         System.out.println("enter the value:");
         value=sc.nextInt();
         System.out.println("value is:"+ value);
-        int [] newarray=new int[n+1];
+        int [] newArray=new int[n+1];
         for(int i=0,j=0;i<n&j<=n;i++,j++){
             if (index == j) {
-                newarray[j] = value;
+                newArray[j] = value;
                 i--;
             }
             if(index!=j){
-                newarray[j]=array[i];
+                newArray[j]=array[i];
             }
         }
-        for(int i=0;i<n+1;i++)
-            System.out.println("new array is :" + newarray[i]);
+        for(int i=0;i<n+1;i++){
+            System.out.println("new array is :" + newArray[i]);
+        }
     }
 
 }
