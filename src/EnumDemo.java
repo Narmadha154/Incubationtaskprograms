a455c885f9e00be069461d2fd97d09a390524f0b
Narmadha154 <85552355+Narmadha154@users.noreply.github.com>
2021-08-03 15:32:34 +0530

    Update EnumDemo.java

--- src/EnumDemo.java
+++ src/EnumDemo.java
@@ -1,25 +1,17 @@
 enum Rainbow{
     VIOLET(1),INDIGO(2),BLUE(3),GREEN(4),YELLOW(5),ORANGE(6),RED(7);
-    int Colorcode;
-    Rainbow(int code){
-      Colorcode=code;
+    private int ColorCode;
+    Rainbow(int ColorCode){
+      this.ColorCode=ColorCode;
     }
-    public int getColorcode(){
-        return Colorcode;
-    }
-    @Override
-    public String toString(){
-        return name().toLowerCase();
+    public int getColorCode(){
+        return this.ColorCode;
     }
 }
 public class EnumDemo{
     public static void main (String [] args){
-        System.out.println("color code of "+ Rainbow.VIOLET+" is "+ Rainbow.VIOLET.getColorcode());
-        System.out.println("color code of "+ Rainbow.INDIGO +" is "+ Rainbow.INDIGO.getColorcode());
-        System.out.println("color code of "+ Rainbow.BLUE +" is "+ Rainbow.BLUE.getColorcode());
-        System.out.println("color code of "+ Rainbow.GREEN +" is "+ Rainbow.GREEN.getColorcode());
-        System.out.println("color code of "+ Rainbow.YELLOW +" is "+ Rainbow.YELLOW.getColorcode());
-        System.out.println("color code of "+ Rainbow.ORANGE +" is "+ Rainbow.ORANGE.getColorcode());
-        System.out.println("color code of "+ Rainbow.RED +" is "+ Rainbow.RED.getColorcode());
+        for(Rainbow color:Rainbow.values()) {
+            System.out.println("color code of " +color+ " is " + color.getColorCode());
+        }
     }
 }
