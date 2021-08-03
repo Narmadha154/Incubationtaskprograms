a4f2f7c1595f86409415ba6f6d3cc0bab975f2e0
Narmadha154 <85552355+Narmadha154@users.noreply.github.com>
2021-08-03 12:10:08 +0530

    Update PojoClass.java

--- src/PojoClass.java
+++ src/PojoClass.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
 
 public class PojoClass {
     public static void main(String[] args) {
-        Employee obj =new Employee("xxx",154,25000,45,"Mumbai");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the employee name:");
+        String name=sc.nextLine();
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("enter the employee id:");
+        int id=sc1.nextInt();
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("enter the employee salary:");
+        Double salary=sc2.nextDouble();
+        Scanner sc3 = new Scanner(System.in);
+        System.out.println("enter the employee age:");
+        int age=sc3.nextInt();
+        Scanner sc4 = new Scanner(System.in);
+        System.out.println("enter the city :");
+        String city=sc4.nextLine();
+        Employee obj =new Employee();
+        obj.setName(name);
+        obj.setId(id);
+        obj.setSalary(salary);
+        obj.setAge(age);
+        obj.setCity(city);
         obj.print();
     }
 
